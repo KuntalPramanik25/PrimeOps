9aade8075132e94776d57f9a54b6e3e2040ab7f6
Anurag <anuragchatterjee165@gmail.com>
2026-07-09 22:39:42 +0530

    CRUD Operation with Table

---
+++ PrimeOps.API/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeOps.DAL;
+using PrimeOps.DAL.Enums;
+using PrimeOps.DAL.Tables;
+
+namespace PrimeOps.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly PrimeOps.DAL.Linq _mapper = new PrimeOps.DAL.Linq();
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _mapper.Select<TItemsMaster>(TableName.TItemsMaster);
+
+            return Ok(result);
+        }
+    }
+}
